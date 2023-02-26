59f7620e019beb4a2a9e6fabda4cbb99b1949762
Rahmarulloh <uzbmv2@gmail.com>
2023-02-26 12:28:45 +0500

    work completed

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
+const card = document.querySelectorAll(".card");
+const color = document.querySelectorAll(".color");
 const code = document.querySelectorAll(".color-code");
+const clg = console.log;
 
-const cards = document.querySelectorAll(".color");
 const colorCode = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'd', 'c', 'e', 'f'];
 
-for (let k = 0; k < 3; k++) {
+for (let i = 0; i < 3; i++) {
     let colorRandom = '#';
-    for (let i = 0; i < 6; i++) {
+
+    for (let j = 0; j < 6; j++) {
       colorRandom += colorCode[Math.floor(Math.random() * colorCode.length)];
     }
-    console.log(colorRandom);
+
+    clg(colorRandom);
     
-    cards[k].style.backgroundColor = colorRandom;
-    console.log(cards[k]);
-    code[k].innerHTML = colorRandom;
-    console.log(code[k].innerHTML = colorRandom);
+    color[i].style.backgroundColor = colorRandom;
+
+    clg(color[i]);
+
+    code[i].innerHTML = colorRandom;
+
+    clg(code[i].innerHTML = colorRandom);
+
+    card[i].addEventListener("click", function () {
+        body.style.backgroundColor = colorRandom;
+        clg(body.style.backgroundColor)
+    })
 }
